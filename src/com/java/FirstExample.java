f5e2e9e43f58aa6a1eb0d9e3ec4287b41bbaf97b
punkhan12 <orkhanrzaev1993@gmail.com>
2022-02-13 11:59:01 -0500

    another change

--- src/com/java/FirstExample.java
+++ src/com/java/FirstExample.java
@@ -9,6 +9,7 @@ public class FirstExample {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("How are you");
+		System.out.println("how was your day?");
 
 	}
 
